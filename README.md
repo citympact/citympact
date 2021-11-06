9006d5273cb876994f03d4377f7ca7a2e0c5a12a
dev@citympact.ch <dev@citympact.ch>
2021-11-06 16:52:25 +0100

    Working on the End-to-end test infrastructure.  - Installed selenium (and provided notes on how to install the chrome    driver in the README.md)  - Using unittest package for a nice output on the command line  - Small updates of the DOM references in the FE (CSS class and id    selectors) for the driving of the test to be easier.  - Implemented a full test of the city project voting. Todo: extend and    finish the testing of the petition voting.

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ Impact project
 ```
 python3 manage.py runserver
 ```
+
+
+## Testing
+Install selenium:
+```
+pip3 install selenium
+```
+
+Download the Chrome driver (`chromedriver`) via https://chromedriver.chromium.org/downloads
+
+For example for Linux and my Chrome version 92:
+```
+cd tests/binaries/
+wget https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+```
---
+++ tests/main.py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+
+import time
+import unittest
+
+WEBAPP_URL = "http://127.0.0.1:8000/"
+
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        options = webdriver.chrome.options.Options()
+        options.add_argument('--headless')
+        service = webdriver.chrome.service.Service("binaries/chromedriver")
+
+        self.browser = webdriver.Chrome(service=service, options=options)
+        self.browser.maximize_window()
+
+    def get_project_first_vote_elt(self, vote_class):
+        try:
+            return self.browser.find_element(
+                By.CLASS_NAME,
+                "project-div"
+            ).find_element(By.CLASS_NAME, vote_class)
+        except:
+            self.fail("Unable to find the upvote element to click on.");
+
+    def test_project_votes(self):
+        # Testing the upvoting and the downvoting on a project:
+        for vote_class in ["upvote", "downvote"]:
+            self.browser.get(WEBAPP_URL)
+            vote_elt = self.get_project_first_vote_elt(vote_class)
+
+            # Making sure the newly vote is displayed as an active class on the
+            # elt:
+            try:
+                vote_elt.click()
+                WebDriverWait(self.browser, 2.5).until(lambda browser:
+                    vote_elt.get_attribute("class") == vote_class+" active-vote"
+                )
+            except TimeoutException as e:
+                self.fail("The voted button did not switch to active");
+
+
+            # Refreshing the page (still with the same session) and making sure
+            # that the registred vote is still displayed:
+            self.browser.refresh();
+            vote_elt = self.get_project_first_vote_elt(vote_class)
+            self.assertEqual(vote_elt.get_attribute("class"),
+                vote_class+" active-vote")
+
+
+    def scrollToElement(self, element):
+        """ Helper function to scroll (and wait on) a given DOM element """
+
+        page_y_offset = self.browser.execute_script("return window.pageYOffset")
+        scroll_offset = element.location["y"] - page_y_offset
+        self.browser.execute_script("window.scrollBy(0, arguments[0]);", scroll_offset)
+
+
+        page_y_offset = self.browser.execute_script("return window.pageYOffset")
+        window_height = self.browser.execute_script("return window.innerHeight")
+        # Waiting that the windows bottom has at least reached the element
+        # y-offset (by substracting the window height of the scrolling offset):
+        WebDriverWait(self.browser, 2.5).until(lambda browser:
+            browser.execute_script("return window.pageYOffset") >= \
+                (scroll_offset - window_height)
+        )
+    def test_petition_five_star_vote(self):
+        # Testing the 5-star voting on a petition:
+        self.browser.get(WEBAPP_URL)
+
+
+        first_project_stars = self.browser.find_elements(
+            By.CSS_SELECTOR, ".petition:first-child .rating-stars>.vote-star")
+
+        for star in first_project_stars:
+            self.scrollToElement(star)
+            star.click()
+            # To do assert impact of the click on the frontend behaviour
+
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
