ed13b2121c27ad20cbda53edc39fdefd1fca4595
dev@citympact.ch <dev@citympact.ch>
2021-11-11 22:55:55 +0100

    - Finished the test case of the add petition - Created a new test case for the search bar - Various other code refactoring

--- tests/main.py
+++ tests/main.py
@@ -1,5 +1,8 @@
+import os
+from PIL import Image, ImageDraw
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -8,7 +11,28 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import unittest
 
-WEBAPP_URL = "http://127.0.0.1:8000/"
+WEBAPP_URL = "http://127.0.0.1:8000"
+
+# New petition representation:
+testPetition = {
+    "title": "New petiton from the test runner",
+    "image": os.path.dirname(os.path.realpath(__file__)) \
+        + "/binaries/testimage.jpg",
+    "description": "This petition was actually filled by a test run."
+}
+
+
+def _parent_element(domElement):
+    """
+    Helper function to find the DOM-parent element of the provided elt.
+    """
+    return domElement.find_element(By.XPATH, "..")
+
+def _has_class(domElement, className):
+    """
+    Helper function to test if the provided element has the provided class
+    """
+    return className in domElement.get_attribute('class').split(" ")
 
 class TestStringMethods(unittest.TestCase):
 
@@ -47,7 +71,7 @@ class TestStringMethods(unittest.TestCase):
         )
 
     def test_project_votes(self):
-        # Testing the upvoting and the downvoting on a project:
+        """ Testing the upvoting and the downvoting on a project. """
         for vote_class in ["upvote", "downvote"]:
             self.browser.get(WEBAPP_URL)
             vote_elt = self.get_project_first_vote_elt(vote_class)
@@ -72,7 +96,7 @@ class TestStringMethods(unittest.TestCase):
 
 
     def test_petition_five_star_vote(self):
-        # Testing the 5-star voting on a petition:
+        """ Testing the 5-star voting on a petition """
         self.browser.get(WEBAPP_URL)
 
 
@@ -107,7 +131,9 @@ class TestStringMethods(unittest.TestCase):
 
 
     def test_add_petition(self):
+        """ Testing the addition of a new petition """
         self.browser.get(WEBAPP_URL)
+
         allAElements = self.browser.find_elements(
             By.CSS_SELECTOR, "a")
         a = list(filter(lambda x: x.text.startswith("Lance une pétition"),
@@ -115,18 +141,106 @@ class TestStringMethods(unittest.TestCase):
 
         self.scrollToElement(a)
         a.click();
+        self.assertIn("petition/add", self.browser.current_url)
 
-        newPetition = {
-            "title": "New petiton from the test runner",
-            "image": "todo...",
-            "description": "This petition was actually filled by a test run."
-        }
-        titleInput = self.browser.find_elements(By.CSS_SELECTOR,
-            "input[name=title]")[0]
-        print("titleInput =", titleInput)
-        titleInput.send_keys(newPetition["title"])
+        self.addNewPetition(testPetition)
+        # We should have been redirected to the home page:
+        self.assertNotIn("petition/add", self.browser.current_url)
 
-        time.sleep(5)
+        # Asserting that the project was correctly added by searching its title:
+        petitionH2 = self.findFirstPetitionH2ByText(testPetition["title"])
+
+        self.assertTrue(petitionH2 is not None)
+
+        # Making sur we have a valid confirmation message:
+        alertDivs = self.browser.find_elements(
+            By.CSS_SELECTOR, ".alert.alert-primary")
+        self.assertTrue(len(alertDivs)>0)
+
+    def test_search_bar(self):
+        """ Testing the behaviour of the search bar """
+        self.browser.get(WEBAPP_URL)
+
+        # Making sure the petition was already saved, if not we quickly add it:
+        if self.findFirstPetitionH2ByText(testPetition["title"]) is None:
+            self.addNewPetition(testPetition)
+
+        search_input = self.browser.find_element(
+            By.CSS_SELECTOR, "#search-input")
+        search_input.send_keys(testPetition["title"][0:3])
+
+
+        proposalsA = _parent_element(search_input).find_element(
+            By.CSS_SELECTOR,
+            ".suggestions-dropdown"
+        ).find_elements(By.CSS_SELECTOR, "ul>li>a")
+
+        # Making sure the test petition is found in the list of proposals
+        found_petition_offset = -1
+        for i, proposalA in enumerate(proposalsA):
+            if testPetition["title"] in proposalA.text:
+                found_petition_offset = i
+                break
+        self.assertTrue(found_petition_offset>=0)
+
+        # Making sure the last proposal is a link to add a new petition:
+        self.assertIn("Propser une pétition", proposalsA[-1].text)
+        self.assertIn("petition/add", proposalsA[-1].get_attribute("href"))
+
+        # Testing the keyboard arrow selection and click behaviour:
+
+        # Making sure the selection circles back correctly
+        for i in range(1 + found_petition_offset + len(proposalsA)):
+            search_input.send_keys(Keys.DOWN)
+
+        targetLi = _parent_element(proposalsA[found_petition_offset])
+        self.assertTrue(_has_class(targetLi, "active-suggestion"))
+
+        # Making sure the click works and redirects to the correct page:
+        targetLi.click()
+
+        self.assertIn("/petition/", self.browser.current_url)
+
+        # Making sure there is an integer petition id part in the url:
+        petitionId = int(self.browser.current_url.split("/petition/")[-1])
+        self.assertTrue(petitionId>=0)
+
+    def findFirstPetitionH2ByText(self, petitionTitle):
+        """
+        Helper function to retrieve a petition title DOM element given content
+        """
+        allPetitionDiv = self.browser.find_elements(
+            By.CSS_SELECTOR, ".petition")
+        for petitionDiv in allPetitionDiv:
+            h2Element = petitionDiv.find_elements(
+                By.CSS_SELECTOR, "h2")[0]
+            if h2Element.text == petitionTitle:
+                return h2Element
+        return None
+
+    def addNewPetition(self, petition):
+        """ Helper function to add a new petition """
+        addPetitionURLSuffix = "/petition/add"
+        if not addPetitionURLSuffix in self.browser.current_url:
+            self.browser.get(WEBAPP_URL+addPetitionURLSuffix)
+
+        # Creating the image in the binary/ adjacent folder:
+        img = Image.new('RGB', (600, 800), color = 'white')
+        d = ImageDraw.Draw(img)
+        d.text((100,200), "Test image", fill=(0,0,0))
+        img.save(petition["image"])
+
+        for k,v in petition.items():
+            inputOrTextarea = self.browser.find_elements(By.CSS_SELECTOR,
+                "input[name=%s], textarea[name=%s]" % (k, k))[0]
+            # FixMe: add robustness to the CSS selection (waiting for better FE)
+
+            inputOrTextarea.send_keys(v)
+
+        submitButton = self.browser.find_elements(By.CSS_SELECTOR, "input[type=submit]")[0]
+        # FixMe: add robustness to the CSS selection (waiting for better FE)
+
+        submitButton.click()
 
 
     def tearDown(self):
